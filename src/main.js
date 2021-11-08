12e38c1aaf3c3040a40999d42ebad8c73a51f270
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-11-08 19:27:32 +0200

    sends request to server to get the phonebook when the page is finished loading

--- src/main.js
+++ src/main.js
@@ -1,31 +1,65 @@
-'use strict'
-import style from './styles.css'
-
+'use strict';
+import style from './styles.css';
 const axios = require('axios');
 
-userActionForm.addEventListener('submit', formAction)
-userAction.addEventListener('input', inputStyling)
+const baseUrl = 'http://localhost:3001/';
 
-function formAction(event){
-    event.preventDefault()
-    console.log(event)
-    if (userAction.value === 'findContact'){
-        console.log('find')
-    }
-    if (userAction.value === 'deleteContact'){
-        console.log('delete')
-    }
-    if (userAction.value === 'addContact'){
-        console.log('add')
-    }
+userActionForm.addEventListener('submit', formAction);
+userAction.addEventListener('input', inputStyling);
+window.addEventListener('load', getPhonebookData);
+
+function formAction(event) {
+  event.preventDefault();
+  console.log(event);
+  if (userAction.value === 'findContact') {
+    console.log('find');
+  }
+  if (userAction.value === 'deleteContact') {
+    console.log('delete');
+    deleteContact()
+  }
+  if (userAction.value === 'addContact') {
+    console.log('add');
+  }
 }
 
-function inputStyling(){
-    if (userAction.value === 'addContact'){
-        inputNameOrId.style["width"] = "25vw";
-        inputPhoneNumber.hidden = false
-    } else {
-        inputNameOrId.style["width"] = "50vw";
-        inputPhoneNumber.hidden = true
-    }
+function inputStyling() {
+  if (userAction.value === 'addContact') {
+    inputNameOrId.style['width'] = '25vw';
+    inputLabelName.textContent = 'Name'
+    inputNameOrId.placeholder = 'Name'
+    inputPhoneNumber.hidden = false;
+  } else {
+    inputNameOrId.style['width'] = '50vw';
+    inputLabelName.textContent = 'Name or ID'
+    inputNameOrId.placeholder = 'Name or ID'
+    inputPhoneNumber.hidden = true;
+  }
+}
+
+function getPhonebookData() {
+  axios.get(`${baseUrl}api/persons`).then((response) => {
+    displayData(response.data);
+  });
+}
+
+function displayData(data) {
+    phonebookInfo.textContent = '';
+  for (const contact in data) {
+    const tableRow = document.createElement('tr');
+    tableRow.append(createTableElement(data[contact].id));
+    tableRow.append(createTableElement(data[contact].name));
+    tableRow.append(createTableElement(data[contact].number));
+    phonebookInfo.append(tableRow);
+  }
+}
+function createTableElement(text) {
+  const tableElement = document.createElement('td');
+  tableElement.textContent = text;
+  return tableElement;
+}
+function deleteContact(){
+    axios.delete(`${baseUrl}api/persons`).then((response) => {
+        displayData(response.data);
+      });
 }
