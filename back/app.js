ad6a17a765c694fc6a1c13219299c4f7f2553882
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-11-10 23:20:05 +0200

    heroku envirment variable

--- back/app.js
+++ back/app.js
@@ -11,6 +11,7 @@ const getPhonebook = require('./routers/getPhonebook')
 const findContact = require('./routers/findContact');
 const deleteContact = require('./routers/deleteContact');
 const addContact = require('./routers/addContact')
+// added heroku environment variable
 
 app.use(morgan('tiny'));
 app.use(cors());
